7d8765070f44733b64a24730cd90e21f5652c33f
MigueBC <mabanos@gmail.com>
2024-09-19 17:09:54 +0200

    - Initial reactive prices-api project setup with hexagonal arch structure and mongo persistence

---
+++ prices-api/src/main/java/com/cap/senior/prices_api/PricesApiApplication.java
@@ -0,0 +1,13 @@
+package com.cap.senior.prices_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesApiApplication.class, args);
+	}
+
+}
---
+++ prices-api/src/test/java/com/cap/senior/prices_api/PricesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.senior.prices_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
